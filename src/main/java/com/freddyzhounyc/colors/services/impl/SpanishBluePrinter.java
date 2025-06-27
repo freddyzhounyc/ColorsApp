2bdbadc9c232aa1861f5807310069d104b12defd
Freddy Zhou <freddyzhou06@gmail.com>
2025-06-27 17:16:22 -0400

    Add spanish support in printing

---
+++ src/main/java/com/freddyzhounyc/colors/services/impl/SpanishBluePrinter.java
@@ -0,0 +1,10 @@
+package com.freddyzhounyc.colors.services.impl;
+
+import com.freddyzhounyc.colors.services.BluePrinter;
+
+public class SpanishBluePrinter implements BluePrinter {
+    @Override
+    public String printBlue() {
+        return "Azul";
+    }
+}

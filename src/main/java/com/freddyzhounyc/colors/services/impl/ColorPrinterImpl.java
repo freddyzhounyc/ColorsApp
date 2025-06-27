37a7b7ded856f2d5edce46f66385ccb251de3550
Freddy Zhou <freddyzhou06@gmail.com>
2025-06-27 17:34:57 -0400

    Remove configuration printer file and replace with the component annotation to serve as beans

--- src/main/java/com/freddyzhounyc/colors/services/impl/ColorPrinterImpl.java
+++ src/main/java/com/freddyzhounyc/colors/services/impl/ColorPrinterImpl.java
@@ -4,8 +4,10 @@ import com.freddyzhounyc.colors.services.BluePrinter;
 import com.freddyzhounyc.colors.services.ColorPrinter;
 import com.freddyzhounyc.colors.services.GreenPrinter;
 import com.freddyzhounyc.colors.services.RedPrinter;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+@Component
 public class ColorPrinterImpl implements ColorPrinter {
     private RedPrinter redPrinter;
     private GreenPrinter greenPrinter;

45b870fb5bece8ae88d7e2027a88ed6015515048
Freddy Zhou <freddyzhou06@gmail.com>
2025-06-27 17:26:13 -0400

    Change printer configuration file

--- src/main/java/com/freddyzhounyc/colors/config/PrinterConfig.java
+++ src/main/java/com/freddyzhounyc/colors/config/PrinterConfig.java
@@ -4,10 +4,7 @@ import com.freddyzhounyc.colors.services.BluePrinter;
 import com.freddyzhounyc.colors.services.ColorPrinter;
 import com.freddyzhounyc.colors.services.GreenPrinter;
 import com.freddyzhounyc.colors.services.RedPrinter;
-import com.freddyzhounyc.colors.services.impl.ColorPrinterImpl;
-import com.freddyzhounyc.colors.services.impl.EnglishBluePrinter;
-import com.freddyzhounyc.colors.services.impl.EnglishGreenPrinter;
-import com.freddyzhounyc.colors.services.impl.EnglishRedPrinter;
+import com.freddyzhounyc.colors.services.impl.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
